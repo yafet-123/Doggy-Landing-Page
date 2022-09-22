bc6d2cee844857db40e9f4fd855a4b05ee4f3878
yafet-123 <yafetaddisu123@gmail.com>
2022-09-22 13:50:36 +0300

    first commit

--- app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import carReducer from '../feature/car/carSlice'
-
-export default configureStore({
-  reducer: {
-    car: carReducer,
-  },
-});
